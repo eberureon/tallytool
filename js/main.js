f8fe6e0b549f0a6d160694cfcd0096ea0b008785
Leon Ebel <ebel.leon@hotmail.com>
2018-05-12 01:46:06 +0200

    burger menu is working but scrolling is weird

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@
 window.onscroll = function() { stickyNav() };
 
 const hamburger = document.querySelector('.hamburger');
-// const mobileNav = document.querySelector('.mobileNavList');
 const navbar = document.querySelector('.navbar');
-const sticky = navbar.offsetTop + 50;
+const sticky = navbar.offsetTop + 150;
 
 function stickyNav() {
   if (window.scrollY >= sticky) {
@@ -13,6 +12,10 @@ function stickyNav() {
     navbar.classList.remove('sticky');
   }
 }
+
+/* TODO: animation auf Mobile veruscht nach unten... scrolling seltsam /
+*  Beim srollen nach oben wird der aktuelle Tab nochmal verschoben nach unten und in den Viewport
+*  gebracht. Tritt erst auf wenn man los laesst. */
 
 // Highlighted Navigation
 $(document).ready(function () {
@@ -29,8 +32,8 @@ $(document).ready(function () {
         $(this).addClass('active');
     
       var target = this.hash,
-          menu = target;
-      $target = $(target);
+        menu = target;
+        $target = $(target);
       $('html, body').stop().animate({
           'scrollTop': $target.offset().top+2
       }, 700, 'swing', function () {
@@ -42,22 +45,23 @@ $(document).ready(function () {
 
 function onScroll(event){
   var scrollPos = $(document).scrollTop();
-  $('#navbar a').each(function () {
+  $('.navbar a').each(function () {
     var currLink = $(this);
     var refElement = $(currLink.attr('href'));
     if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-      $('#navbar ul a').removeClass('active');
+      $('.navbar ul a').removeClass('active');
       currLink.addClass('active');
     }
   });
 }
 
-// Burger Menu
+// Burger Menu 'X' animation
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('is-active');
-  // if (document.querySelector('.mobileNavList.hidden')) {
-  //   mobileNav.classList.toggle('hidden');
-  // } else {
-  //   mobileNav.classList.toggle('hidden');
-  // }
+});
+
+// Close Burger Menu when Link clicked
+$('a[href^="#"]').on('click', function () {
+    $('.hamburger').removeClass('is-active');
+    $('.navbar ul').removeClass('show');
 });
