e55ba6f61329d21bae17c8f317fba889c3337b88
Leon Ebel <ebel.leon@hotmail.com>
2018-05-22 00:39:13 +0200

    navbar without bootstrap remove bootstrap MISSING transition

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.onscroll = function() { stickyNav() };
 const $href = $('a[href^="#"]');
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
+const mobileNavList = document.querySelector('.mobileNavList');
 const sticky = navbar.offsetTop + 150;
 
 function stickyNav() {
